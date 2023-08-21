5689b4bf4ff72c01b62ad51633b72ee753ae4825
wyllierosi <wyllierosi@gs2108.AD.ECS.VUW.AC.NZ>
2023-08-21 15:55:34 +1200

    test draft

--- src/net/andyfoster/bugworld/Test.java
+++ src/net/andyfoster/bugworld/Test.java
@@ -2,4 +2,11 @@ package net.andyfoster.bugworld;
 
 public class Test {
 
+	public static void main(String[] args) {
+
+		b1name = b1.getName();
+		system.out.println(b1name);
+		b1.toString();
+
+	}
 }
