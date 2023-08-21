f026d586f451c210324c0dd2c615888bc359753a
fosterandr <fosterandr@gs2304.AD.ECS.VUW.AC.NZ>
2023-08-21 14:18:32 +1200

    Initial commit

---
+++ src/net/andyfoster/bugworld/BugWorld.java
@@ -0,0 +1,15 @@
+package net.andyfoster.bugworld;
+
+public class BugWorld {
+
+	public BugWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to BugWorld!");
+
+	}
+
+}
