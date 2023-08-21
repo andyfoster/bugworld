92a31483bfe4920ed0451214891cfdf3a1f87fdd
wyllierosi <wyllierosi@gs2108.AD.ECS.VUW.AC.NZ>
2023-08-21 14:54:51 +1200

    Test class added

--- src/net/andyfoster/bugworld/BugWorld.java
+++ src/net/andyfoster/bugworld/BugWorld.java
@@ -9,6 +9,7 @@ public class BugWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to BugWorld!");
+		System.out.println("Test by Rosie");
 
 	}
 
