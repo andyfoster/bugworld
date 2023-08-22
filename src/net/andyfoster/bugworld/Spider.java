47f70a7103efbe4f9785bceec62304e2fd25ef66
Andy Foster <andrewfosteris@gmail.com>
2023-08-22 15:22:18 +1200

    Create Spider.java

---
+++ src/net/andyfoster/bugworld/Spider.java
@@ -0,0 +1,14 @@
+package net.andyfoster.bugworld;
+
+public class Spider extends Bug {
+
+	public Spider(String name, String species, char symbol, int x, int y, int energy, int id) {
+		super(name, "Spider", symbol, x, y, energy, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Spider() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
