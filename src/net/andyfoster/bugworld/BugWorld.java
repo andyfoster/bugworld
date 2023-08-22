89cb9859183d65d1097ae91e232926630866d811
Andy Foster <andrewfosteris@gmail.com>
2023-08-22 15:22:42 +1200

    Comment out Scanner, move north

--- src/net/andyfoster/bugworld/BugWorld.java
+++ src/net/andyfoster/bugworld/BugWorld.java
@@ -2,7 +2,6 @@ package net.andyfoster.bugworld;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class BugWorld {
 
@@ -12,30 +11,39 @@ public class BugWorld {
 		Bug b1 = new Bug();
 		Bug b2 = new Bug("Barry", "Beetle", 'B', 100, 100, 23, 4);
 
-		System.out.println(b1.toString());
-		System.out.println(b2.toString());
+//		System.out.println(b1.toString());
+//		System.out.println(b2.toString());
+//
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("x: ");
+//		int x = sc.nextInt();
+//		System.out.print("y ");
+//		int y = sc.nextInt();
+//		System.out.print("energy: ");
+//		int energy = sc.nextInt();
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("x: ");
-		int x = sc.nextInt();
-		System.out.print("y ");
-		int y = sc.nextInt();
-		System.out.print("energy: ");
-		int energy = sc.nextInt();
+//		Bug b3 = new Bug("Bertie", "Bee", 'B', x, y, energy, 100);
 
-		Bug b3 = new Bug("Bertie", "Bee", 'B', x, y, energy, 100);
+		Spider b4 = new Spider();
+		b4.setName("Sam");
 
-		System.out.println(b3.toText());
+//		System.out.println(b3.toText());
 
-		List<Bug> bugs = new ArrayList<Bug>();
+		List<Bug> bugs = new ArrayList<>();
 
 		bugs.add(b1);
 		bugs.add(b2);
-		bugs.add(b3);
+//		bugs.add(b3);
+		bugs.add(b4);
 
 		for (Bug b : bugs) {
-			b.toText();
+			System.out.println(b.toText());
 		}
+
+		b1.move("N");
+		b1.move("N", 5);
+
+		System.out.println(b1.toText());
 
 	}
 }
