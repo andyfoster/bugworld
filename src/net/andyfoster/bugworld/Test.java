0c8faa829cf6b5651fc70e80ca950949bd829ace
wyllierosi <wyllierosi@gs2108.AD.ECS.VUW.AC.NZ>
2023-08-22 13:28:30 +1200

    test for task 1 ready

--- src/net/andyfoster/bugworld/Test.java
+++ src/net/andyfoster/bugworld/Test.java
@@ -2,12 +2,33 @@ package net.andyfoster.bugworld;
 
 public class Test {
 
-	public static void main(String[] args) {
+	private String b1name;
+	private String b2name;
 
-		Bug b1 = new Bug();
-		String b1name = b1.getName();
-		System.out.println(b1name);
-		b1.toString();
+	// the constructor test is used to call
+	public Test() {
+		this.testOne();
 
 	}
+
+	public static void main(String[] args) {
+		new Test();
+	}
+
+	// method testing task 1
+	public void testOne() {
+		// test for constructor 1
+		Bug b1 = new Bug();
+		b1name = " ";
+		b1name = b1.getName();
+		System.out.println(b1name);
+		System.out.println(b1.toString());
+		System.out.println(b1.toText());
+
+		// test for constructor 2
+		Bug b2 = new Bug("Human", "Rosie", 'R', 10, 10, 100, 100);
+		System.out.println(b2);
+		System.out.println(b2.toText());
+	}
+
 }
