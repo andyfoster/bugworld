b8fa32c834b149828dca26e20b3d72d1ee863a0d
Andy Foster <andrewfosteris@gmail.com>
2023-08-23 16:45:52 +1200

    Create a world and update it

--- src/net/andyfoster/bugworld/BugWorld.java
+++ src/net/andyfoster/bugworld/BugWorld.java
@@ -1,18 +1,24 @@
 package net.andyfoster.bugworld;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.lang.Thread.sleep;
 
 public class BugWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to BugWorld!");
 
         World w = new World();
         w.drawWorld();
 
-        Bug b1 = new Bug();
-        Bug b2 = new Bug("Barry", "Beetle", 'B', 100, 100, 23, 4);
+        sleep(1000);
+        w.updateWorld();
+        w.drawWorld();
+
+//        System.in;
+
+
+//        Bug b1 = new Bug();
+//        Bug b2 = new Bug("Barry", "Beetle", 'B', 100, 100, 23, 4);
 
 //		System.out.println(b1.toString());
 //		System.out.println(b2.toString());
@@ -27,24 +33,24 @@ public class BugWorld {
 
 //		Bug b3 = new Bug("Bertie", "Bee", 'B', x, y, energy, 100);
 
-        Spider b4 = new Spider();
-        b4.setName("Sam");
+//        Spider b4 = new Spider();
+//        b4.setName("Sam");
+//
+////		System.out.println(b3.toText());
+//
+//        List<Bug> bugs = new ArrayList<>();
 
-//		System.out.println(b3.toText());
-
-        List<Bug> bugs = new ArrayList<>();
-
-        bugs.add(b1);
-        bugs.add(b2);
-//		bugs.add(b3);
-        bugs.add(b4);
-
-        for (Bug b : bugs) {
-//			System.out.println(b.toText());
-        }
-
-        b1.move("N");
-        b1.move("N", 5);
+//        bugs.add(b1);
+//        bugs.add(b2);
+////		bugs.add(b3);
+//        bugs.add(b4);
+//
+//        for (Bug b : bugs) {
+////			System.out.println(b.toText());
+//        }
+//
+//        b1.move("N");
+//        b1.move("N", 5);
 
 //		System.out.println(b1.toText());
 
