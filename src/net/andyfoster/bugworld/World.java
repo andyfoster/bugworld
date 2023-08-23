427f41dce179ca19a431ff5df6dd246b48cedfa5
Andy Foster <andrewfosteris@gmail.com>
2023-08-24 11:14:45 +1200

    feat: Add obstacles to the bug world
    This commit adds functionality to the bug world by introducing obstacles. Obstacles are now stored in a separate ArrayList and can be placed on specific coordinates within the world. The drawWorld() method has been updated to display obstacles as well. Additionally, a new method updateWorld() has been added which randomly moves bugs within the world and prints their movements.

--- src/net/andyfoster/bugworld/World.java
+++ src/net/andyfoster/bugworld/World.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class World {
 
     private final ArrayList<Bug> bugs;
+    private final ArrayList<Obstacle> obstacles;
     private final int height = 10;
     private final int width = 10;
 
@@ -13,11 +14,22 @@ public class World {
 
         bugs.add(new Bug("Steve", "Spider", 'S', 5, 4, 30, 1));
         bugs.add(new Bug("Charles", "Cockroach", 'C', 3, 9, 30, 2));
+
+        obstacles = new ArrayList<>();
+        obstacles.add(new Obstacle("Rock", 'R', 2, 2));
+        obstacles.add(new Obstacle("Leaf", 'L', 5, 5));
     }
 
     public void drawWorld() {
         StringBuilder sb = new StringBuilder();
-        sb.append("|--------------------|\n");
+        System.out.print('+');
+        for (int i = 0; i < width; i++)
+            System.out.print('-');
+        System.out.println('+');
+
+        this.bugs.sort(new BugComparator());
+
+//        sb.append("|--------------------|\n");
 
         for (int y = 0; y < height; y++) {
 
@@ -30,6 +42,13 @@ public class World {
                         sb.append(" ");
                     }
                 }
+                for (Obstacle o : obstacles) {
+                    if (o.x() == x && o.y() == y) {
+                        sb.append(o.symbol());
+                    } else {
+                        sb.append(" ");
+                    }
+                }
             }
             sb.append("|\n");
         }
@@ -38,4 +57,27 @@ public class World {
 
     }
 
+    public void updateWorld() {
+        String chosenDirection;
+        int distance = 5;
+
+        for (Bug b : bugs) {
+            chosenDirection = randomDirection();
+
+            if (b.getY() > height || b.getY() <= 0) {
+                // nothing
+//                System.out.println();
+                System.out.println("Bug " + b.getName() + " is at the edge of the world");
+            } else {
+                b.move(chosenDirection, distance);
+                System.out.println("Bug " + b.getName() + " moved " + chosenDirection + " " + distance + " spaces");
+            }
+        }
+    }
+
+    public String randomDirection() {
+        String[] directionsArray = {"N", "S", "E", "W"};
+        int randomIndex = (int) (Math.random() * directionsArray.length);
+        return directionsArray[randomIndex];
+    }
 }
