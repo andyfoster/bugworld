70f373283617491da9c050c6810bafb29ce8ead2
Andy Foster <andrewfosteris@gmail.com>
2023-08-24 11:23:11 +1200

    Add Plant class to Bug World
    This commit adds the Plant class to the Bug World project. The Plant class represents a plant in the bug world and includes properties such as its position (x, y) and size. It also includes getter and setter methods for accessing and modifying the size property of a plant.

---
+++ src/net/andyfoster/bugworld/Plant.java
@@ -0,0 +1,29 @@
+package net.andyfoster.bugworld;
+
+public class Plant {
+    private final int x;
+    private final int y;
+    private int size;
+
+    public Plant(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
