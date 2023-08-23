facfff8642e2063ddd60cad116268ca94587cac7
Andy Foster <andrewfosteris@gmail.com>
2023-08-24 11:04:35 +1200

    Rename Obstacles and change it to a Record

---
+++ src/net/andyfoster/bugworld/Obstacle.java
@@ -0,0 +1,27 @@
+package net.andyfoster.bugworld;
+
+public record Obstacle(String name, char symbol, int x, int y) {
+
+}
+//
+//public class Obstacles {
+//    private final String name;
+//    private final char symbol;
+//    private final int x;
+//    private final int y;
+//
+//    public Obstacles(String name, char symbol, int x, int y) {
+//        this.name = name;
+//        this.symbol = symbol;
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    public int getX() {
+//        return x;
+//    }
+//
+//    public int getY() {
+//        return y;
+//    }
+//}
