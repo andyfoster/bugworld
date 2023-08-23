3b941fa5d45cb4a53dacd74561c0774a1c89b8fb
Andy Foster <andrewfosteris@gmail.com>
2023-08-24 11:40:47 +1200

    Refactor Obstacle class to extend GameObject
    The Obstacle class has been refactored to extend the GameObject class. This change replaces the previous record implementation with a new class that includes getter methods for x and y coordinates.

--- src/net/andyfoster/bugworld/Obstacle.java
+++ src/net/andyfoster/bugworld/Obstacle.java
@@ -1,27 +1,25 @@
 package net.andyfoster.bugworld;
 
-public record Obstacle(String name, char symbol, int x, int y) {
+//public record Obstacle(String name, char symbol, int x, int y) extends GameObject {}
 
+public class Obstacle extends GameObject {
+    private final String name;
+    private final char symbol;
+    private final int x;
+    private final int y;
+
+    public Obstacle(String name, char symbol, int x, int y) {
+        this.name = name;
+        this.symbol = symbol;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
-//
-//public class Obstacles {
-//    private final String name;
-//    private final char symbol;
-//    private final int x;
-//    private final int y;
-//
-//    public Obstacles(String name, char symbol, int x, int y) {
-//        this.name = name;
-//        this.symbol = symbol;
-//        this.x = x;
-//        this.y = y;
-//    }
-//
-//    public int getX() {
-//        return x;
-//    }
-//
-//    public int getY() {
-//        return y;
-//    }
-//}
